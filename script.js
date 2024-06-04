17461d8bbb34dcd7d250b89c702ceed738948609
DACAS666 <dacas666@gmail.com>
2024-06-04 17:54:43 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var canvas = document.getElementById('signaturePad');
+    var clearButton = document.getElementById('clear');
+    var ctx = canvas.getContext('2d');
+    var drawing = false;
+
+    function startDrawing(e) {
+        drawing = true;
+        draw(e);
+    }
+
+    function endDrawing() {
+        drawing = false;
+        ctx.beginPath();
+        document.getElementById('signature').value = canvas.toDataURL();
+    }
+
+    function draw(e) {
+        if (!drawing) return;
+        ctx.lineWidth = 2;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'black';
+
+        ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    }
+
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mouseup', endDrawing);
+    canvas.addEventListener('mousemove', draw);
+    clearButton.addEventListener('click', function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    });
+
+    document.getElementById('contractForm').addEventListener('submit', function(e) {
+        document.getElementById('signature').value = canvas.toDataURL();
+    });
+});
